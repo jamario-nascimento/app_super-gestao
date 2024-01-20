9c540d8210511fafedf199381f971d4ded4affb3
Jamário Nascimento <jamariobatista@gmail.com>
2024-01-20 15:10:23 -0300

    Project files

---
+++ README.md
@@ -0,0 +1,40 @@
+<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
+
+<p align="center">
+<a href="https://travis-ci.org/laravel/framework"><img src="https://travis-ci.org/laravel/framework.svg" alt="Build Status"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/d/total.svg" alt="Total Downloads"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/v/stable.svg" alt="Latest Stable Version"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/license.svg" alt="License"></a>
+</p>
+
+## Learning Laravel
+
+[documentation](https://laravel.com/docs)
+
+[Laracasts](https://laracasts.com)
+
+## Sobre o projeto
+
+Sistema de gestão...
+
+- [Simple, fast routing engine](https://laravel.com/docs/routing).
+
+### Premium Partners
+
+- **[Vehikl](https://vehikl.com/)**
+- **[Tighten Co.](https://tighten.co)**
+- **[Kirschbaum Development Group](https://kirschbaumdevelopment.com)**
+- **[64 Robots](https://64robots.com)**
+- **[Cubet Techno Labs](https://cubettech.com)**
+- **[Cyber-Duck](https://cyber-duck.co.uk)**
+- **[Many](https://www.many.co.uk)**
+- **[Webdock, Fast VPS Hosting](https://www.webdock.io/en)**
+- **[DevSquad](https://devsquad.com)**
+- **[OP.GG](https://op.gg)**
+
+### Comandos
+
+- ```php artisan -V``` **Mostra a versão do Laravel**
+- ```php artisan serve``` **Sobe a aplição em um servidor próprio, usado para dev**
+- ```php artisan list``` **Mostra a lista de comandos do artesão**
+- ```php artisan route:list``` **Mostra a lista de rottas da aplicação**
---
+++ routes/web.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+| Verbos http
+| get
+| post
+| put
+| patch
+| delete
+| options
+*/
+
+//Route::get('/', function () {
+//    return 'Olá, seja bem vindo!';
+//});
+
+Route::get('/','PrincipalController@principal')->name('site.index');
+
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobre-nos');
+
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+
+Route::get('/login', function (){ return 'Login';})->name('site.login');
+// Agrupando rotas
+Route::prefix('/app')->group(function (){
+    Route::get('/clientes', function (){
+        return 'Clientes';
+    })->name('app.clientes');
+
+    Route::get('/fornecedores', function (){
+        return 'fornecedores';
+    })->name('app.fornecedores');
+
+    Route::get('/produtos', function (){
+        return 'produtos';
+    })->name('app.produtos');
+});
+
+//Redirecionamento de rotas
+Route::get('/rota1', function (){
+    echo 'rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function (){
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+//Route::redirect('/rota2','/rota1');
+
+// A Rota fallback usada para customizar a rota de erro padrão do laravel
+Route::fallback( function (){
+   echo 'A rota acessada não existe. <a href="'.route('site.index').'"> clique aqui</a> para voltar a página inicial.';
+});
+//Passando Parametros nas rotas
+Route::get('/contato/{nome}/{categoria_id}', function (string $nome, int $categoria = 1){
+    echo "Estamos aqui: $nome - $categoria";
+})
+    ->where('categoria_id','[0-9]+')
+    ->where('nome','[A-Za-z]+');
+
