62e19a33a03d993b4b385e5bc04d4b6b988ceb1f
Jamário Nascimento <jamariobatista@gmail.com>
2024-01-20 16:22:28 -0300

    understend blade

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,16 @@
+<h3>Fornecedor</h3>
+
+{{-- Comentário que será descartado pelo imtepretador balde --}}
+@php
+// Para comentarios de uma linha
+/*
+ * Para comentario de
+ * multiplas linhas
+ * */
+
+echo 'teste';
+@endphp
+
+{{ 'Teste' }}
+
+<?= 'teste' ?>
--- routes/web.php
+++ routes/web.php
@@ -36,9 +36,7 @@ Route::prefix('/app')->group(function (){
         return 'Clientes';
     })->name('app.clientes');
 
-    Route::get('/fornecedores', function (){
-        return 'fornecedores';
-    })->name('app.fornecedores');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
 
     Route::get('/produtos', function (){
         return 'produtos';
