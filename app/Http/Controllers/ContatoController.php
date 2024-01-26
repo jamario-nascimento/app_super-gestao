aacb3ffe29aa27fdb8edb5a0cfbab5a2dc666b93
Jamário Nascimento <jamariobatista@gmail.com>
2024-01-26 13:14:01 -0300

    request http

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -6,9 +6,13 @@ use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato()
+    public function contato(Request $request)
     {
-        var_dump($_POST);
+//        dd($request->request->all());
+        echo'<pre>';
+        print_r($request->all());
+        echo $request->input('nome');
+
         return view('site.contato',['titulo'=>'Contato']);
     }
 }
