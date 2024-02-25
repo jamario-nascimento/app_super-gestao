c29f513d094b86bde4473a7d0d1aaf9162bfa75e
Jamário Nascimento <jamariobatista@gmail.com>
2024-02-25 14:24:05 -0300

    Validando dados acesso formulario

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,7 +11,19 @@ class LoginController extends Controller
         return view('site.login',['titulo'=>'Login']);
     }
 
-    public function autenticar() {
-        echo 'Chegamos até aqui';
+    public function autenticar(Request $request) {
+       $regras = [
+           'usuario' => 'email|required',
+           'senha' => 'required'
+       ];
+
+       $feedback = [
+           'usuario.email' => 'O campo usuário (e-mail) é obrigatório',
+           'senha.required' => 'O campo senha é obrigatório'
+       ];
+
+       $request->validate($regras, $feedback);
+
+       print_r($request->all());
     }
 }
