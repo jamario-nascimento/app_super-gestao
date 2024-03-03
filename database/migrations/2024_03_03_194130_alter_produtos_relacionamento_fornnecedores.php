97795fdc09b51a83c15bfa56349e126e8cf09c44
Jamário Nascimento <jamariobatista@gmail.com>
2024-03-03 17:17:55 -0300

    criando migration fornecedor produto

---
+++ database/migrations/2024_03_03_194130_alter_produtos_relacionamento_fornnecedores.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AlterProdutosRelacionamentoFornnecedores extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('produtos', function (Blueprint $table) {
+          $fornecedor_id =  DB::table('fornecedores')->insertGetId([
+                'nome' => 'John Doe',
+                'site' => 'https://johndoe.com',
+                'uf' => 'SP',
+                'email' => 'contact@johndoe.com'
+            ]);
+            $table->unsignedBigInteger('fornecedor_id')->default($fornecedor_id)->after('id');
+            $table->foreign('fornecedor_id')->references('id')->on('fornecedores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('produtos', function (Blueprint $table) {
+        $table->dropForeign('produtos_fornecedor_id_foreign');
+        $table->dropColumn('fornecedor_id');
+    });
+    }
+}
