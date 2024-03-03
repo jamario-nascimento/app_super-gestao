56f2979e14e781287374571f1aa033e9cb21ddf2
Jamário Nascimento <jamariobatista@gmail.com>
2024-03-03 13:54:00 -0300

    implementado hasOne Eloquent ORM

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -17,15 +17,16 @@ class ProdutoController extends Controller
     public function index(Request $request)
     {
         $produtos =  Produto::paginate(10);
-        foreach ($produtos as $key => $produto) {
-            $produtoDetalhe = ProdutoDetalhe::where('produto_id', $produto->id)->first();
 
-            if (isset($produtoDetalhe)) {
-                $produtos[$key]['comprimento'] = $produtoDetalhe->comprimento;
-                $produtos[$key]['largura'] = $produtoDetalhe->largura;
-                $produtos[$key]['altura'] = $produtoDetalhe->altura;
-            }
-        }
+        // foreach ($produtos as $key => $produto) {
+        //     $produtoDetalhe = ProdutoDetalhe::where('produto_id', $produto->id)->first();
+
+        //     if (isset($produtoDetalhe)) {
+        //         $produtos[$key]['comprimento'] = $produtoDetalhe->comprimento;
+        //         $produtos[$key]['largura'] = $produtoDetalhe->largura;
+        //         $produtos[$key]['altura'] = $produtoDetalhe->altura;
+        //     }
+        // }
 
         return view('app.produto.index', ['produtos' => $produtos, 'request' => $request->all()]);
     }
--- app/Produto.php
+++ app/Produto.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     protected $fillable = ['nome', 'descricao', 'peso', 'unidade_id'];
+
+    /* 
+        Relacionamento um para um usando Eloguent ORM 
+        Registro relacionado em produto_detalhes (fk) -> produto_id
+        O mapeamento ocorre usando a convenção de nomes de colunas primary e foreign key
+    */
+    public function produtoDetalhe()
+    {
+        return $this->hasOne('App\ProdutoDetalhe');
+    }
 }
